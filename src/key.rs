076804f31bac98fb33a18fa975686000d7edd99b
Monroe Clinton <contact@monroeclinton.com>
2022-05-29 14:07:47 -0400

    grab_key function

---
+++ src/key.rs
@@ -0,0 +1,19 @@
+pub fn grab_key(conn: &xcb::Connection, modifier: u16, keysym: u32, root_window: xcb::Window) {
+    let key_symbols = xcb_util::keysyms::KeySymbols::new(conn);
+    match key_symbols.get_keycode(keysym).next() {
+        Some(keycode) => {
+            xcb::grab_key(
+                conn,
+                false,
+                root_window,
+                modifier,
+                keycode,
+                xcb::GRAB_MODE_ASYNC as u8,
+                xcb::GRAB_MODE_ASYNC as u8,
+            );
+        }
+        _ => {
+            dbg!("Failed to find keycode for keysym: {}", keysym);
+        }
+    }
+}
