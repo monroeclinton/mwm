72792dfcafbcf32e3f829d788c0ba4a16ada122a
Monroe Clinton <contact@monroeclinton.com>
2022-06-19 23:13:48 -0400

    Add time/mem to statusbar

--- statusbar/src/draw.rs
+++ statusbar/src/draw.rs
@@ -1,10 +1,12 @@
 use crate::config::Config;
 use std::sync::Arc;
+use systemstat::{System, Platform, saturating_sub_bytes};
 
 pub struct Draw {
     xcb_conn: Arc<xcb_util::ewmh::Connection>,
     config: Arc<Config>,
     surface: cairo::XCBSurface,
+    context: cairo::Context,
 }
 
 impl Draw {
@@ -43,10 +45,14 @@ impl Draw {
             40
         ).expect("Unable to create Cairo surface.");
 
+        let context = cairo::Context::new(&surface)
+            .expect("Unable to find context of statusbar surface.");
+
         Self {
             xcb_conn,
             config,
             surface,
+            context,
         }
     }
 
@@ -59,60 +65,7 @@ impl Draw {
 
         self.bar_title();
         self.workspaces();
-    }
-
-    fn bar_title(&self) {
-        let reply = xcb_util::ewmh::get_active_window(
-            &self.xcb_conn,
-            0
-        ).get_reply();
-
-        let window_name = if let Ok(active_window) = reply {
-            let reply = xcb_util::ewmh::get_wm_name(
-                &self.xcb_conn,
-                active_window
-            ).get_reply();
-
-            match reply {
-                Ok(name) => Some(name.string().to_string()),
-                _ => None,
-            }
-        } else {
-            None
-        };
-
-        let title = if let Some(name) = window_name {
-            format!("[{}] {}@{}", name, whoami::username(), whoami::hostname())
-        } else {
-            format!("{}@{}", whoami::username(), whoami::hostname())
-        };
-
-        let screen = self.xcb_conn.get_setup().roots().next()
-            .expect("Unable to find a screen.");
-
-        let context = cairo::Context::new(&self.surface)
-            .expect("Unable to find context of statusbar surface.");
-
-        let font_face = cairo::FontFace::toy_create(
-            self.config.font_family.as_str(),
-            cairo::FontSlant::Normal,
-            cairo::FontWeight::Normal
-        ).expect("Unable to create font face in statusbar.");
-
-        let extents = context.text_extents(title.as_str())
-            .expect("Unable to find text text extents of statusbar workspace.");
-
-        context.set_font_face(&font_face);
-        context.set_font_size(self.config.font_size as f64);
-        context.move_to(
-            (screen.width_in_pixels() as f64 - extents.width) / 2.0,
-            (self.config.height as f64 + extents.height) / 2.0
-        );
-
-        set_source_rgb(&context, self.config.font_color);
-
-        context.show_text(title.as_str())
-            .expect("Cannot position text on surface in statusbar.");
+        self.draw_info();
     }
 
     fn workspaces(&self) {
@@ -128,8 +81,7 @@ impl Draw {
             0
         ).get_reply().unwrap_or(0) as usize;
 
-        let context = cairo::Context::new(&self.surface)
-            .expect("Unable to find context of statusbar surface.");
+        let context = &self.context;
 
         let font_face = cairo::FontFace::toy_create(
             self.config.font_family.as_str(),
@@ -182,6 +134,116 @@ impl Draw {
 
         self.surface.flush();
         self.xcb_conn.flush();
+    }
+
+    fn bar_title(&self) {
+        let reply = xcb_util::ewmh::get_active_window(
+            &self.xcb_conn,
+            0
+        ).get_reply();
+
+        let window_name = if let Ok(active_window) = reply {
+            let reply = xcb_util::ewmh::get_wm_name(
+                &self.xcb_conn,
+                active_window
+            ).get_reply();
+
+            match reply {
+                Ok(name) => Some(name.string().to_string()),
+                _ => None,
+            }
+        } else {
+            None
+        };
+
+        let title = if let Some(name) = window_name {
+            format!("[{}] {}@{}", name, whoami::username(), whoami::hostname())
+        } else {
+            format!("{}@{}", whoami::username(), whoami::hostname())
+        };
+
+        let screen = self.xcb_conn.get_setup().roots().next()
+            .expect("Unable to find a screen.");
+
+        let context = &self.context;
+
+        let font_face = cairo::FontFace::toy_create(
+            self.config.font_family.as_str(),
+            cairo::FontSlant::Normal,
+            cairo::FontWeight::Normal
+        ).expect("Unable to create font face in statusbar.");
+
+        let extents = context.text_extents(title.as_str())
+            .expect("Unable to find text text extents of statusbar workspace.");
+
+        context.set_font_face(&font_face);
+        context.set_font_size(self.config.font_size as f64);
+        context.move_to(
+            (screen.width_in_pixels() as f64 - extents.width) / 2.0,
+            (self.config.height as f64 + extents.height) / 2.0
+        );
+
+        set_source_rgb(&context, self.config.font_color);
+
+        context.show_text(title.as_str())
+            .expect("Cannot position text on surface in statusbar.");
+    }
+
+    fn draw_info(&self) {
+        let sys = System::new();
+
+        let memory = match sys.memory() {
+            Ok(mem) => {
+                let used_memory = saturating_sub_bytes(mem.total, mem.free);
+                format!(
+                    "Mem: {} ({:.0}% used)",
+                    used_memory,
+                    (used_memory.as_u64() as f64 / mem.total.as_u64() as f64) * 100.0,
+                )
+            },
+            _ => format!("Mem: Memory error.")
+        };
+
+        let date = format!(
+            "Time: {}",
+            chrono::Local::now().format("%b %d (%a) %I:%M%p")
+        );
+
+        let blocks = vec![memory, date];
+
+        let screen = self.xcb_conn.get_setup().roots().next()
+            .expect("Unable to find a screen.");
+
+        let context = &self.context;
+
+        let font_face = cairo::FontFace::toy_create(
+            self.config.font_family.as_str(),
+            cairo::FontSlant::Normal,
+            cairo::FontWeight::Normal
+        ).expect("Unable to create font face in statusbar.");
+
+        let bar_height = self.config.height as f64;
+        let bar_width = screen.width_in_pixels() as f64 - (self.config.margin * 2) as f64;
+        let mut offset= 0.0;
+
+        for text in blocks {
+            let extents = context.text_extents(text.as_str())
+                .expect("Unable to find text text extents of statusbar workspace.");
+
+            context.set_font_face(&font_face);
+            context.set_font_size(self.config.font_size as f64);
+            context.move_to(
+                bar_width - offset - extents.width,
+                (bar_height + extents.height) / 2.0
+            );
+
+            set_source_rgb(&context, self.config.font_color);
+
+            context.show_text(text.as_str())
+                .expect("Cannot position text on surface in statusbar.");
+
+            offset += extents.width + 20.0;
+        }
     }
 }
 
